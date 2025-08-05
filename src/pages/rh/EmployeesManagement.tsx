c6fa05a0cb8d79bfb533e4316c7f638fc8709fcf
Michael Agisha <agishazigabemichael@gmail.com>
2025-08-05 03:05:56 +0200

    Add complete employee management form with CRUD operations and post-nom field

--- src/pages/rh/EmployeesManagement.tsx
+++ src/pages/rh/EmployeesManagement.tsx
@@ -1,12 +1,1 @@
-import React from "react";
-
-const EmployeesManagement: React.FC = () => {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-gray-800">Gestion des Employés</h1>
-      <p className="text-gray-600">Page en cours de développement...</p>
-    </div>
-  );
-};
-
-export default EmployeesManagement;
+ 
